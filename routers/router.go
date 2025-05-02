a9fd023b66bcfff6c7a56916681bff4c7d45b4d8
xieyangyuyue <1483618794@qq.com>
2025-05-02 15:10:18 +0800

    [add] 编写博客文章接口（新增、编辑、删除）

--- routers/router.go
+++ routers/router.go
@@ -20,26 +20,24 @@ func InitRouter() {
 	// 所有路由将以 /api/v1/ 作为前缀
 	router := r.Group("api/v1")
 	{
-		// 后续可以在此处添加更多路由：
-		// router.POST("/articles", 创建文章)
-		// router.GET("/articles", 获取文章列表)
-		// router.PUT("/articles/:id", 更新文章)
-
 		//用户模块的路由接口
 		router.POST("user/add", v1.AddUser)
 		router.GET("users", v1.GetUsers)
 		router.PUT("user/:id", v1.EditUser)
 		router.DELETE("user/:id", v1.DeleteUser)
 
-		//分类模块的路由接口
 		// 分类模块的路由接口
 		router.GET("category", v1.GetCate)
 		router.GET("category/:id", v1.GetCateInfo)
 		router.POST("category/add", v1.AddCategory)
 		router.PUT("category/:id", v1.EditCate)
 		router.DELETE("category/:id", v1.DeleteCate)
-		//文章模块的路由接口
 
+		//文章模块的路由接口
+		router.GET("article", v1.GetArt)
+		router.POST("article/add", v1.AddArticle)
+		router.PUT("article/:id", v1.EditArt)
+		router.DELETE("article/:id", v1.DeleteArt)
 	}
 
 	// 启动HTTP服务（从配置中读取端口号）
